c7cb5fc8ea970164a9719de9f44e96bdfb1c263a
Naufal Athallah Iwel <spealdm6@gmail.com>
2023-09-07 10:52:01 +0700

    feat: config wdio running appium on emulator android

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,6 +1,6 @@
 const path = require("path");
 
-export const config = {
+exports.config = {
   //
   // ====================
   // Runner Configuration
@@ -55,9 +55,9 @@ export const config = {
     {
       // capabilities for local Appium web tests on an Android Emulator
       "appium:platformName": "Android",
-      "appium:deviceName": "Nexus 5 Tiramisu",
-      "appium:platformVersion": "12.0",
-      "appium:automationName": "UiAutomator2",
+      "appium:deviceName": "Nexus 5 API 33 Tiramisu",
+      "appium:platformVersion": "13",
+      "appium:automationName": "UIAutomator2",
       "appium:app": path.join(process.cwd(), "app/android/ApiDemos-debug.apk"),
     },
   ],
