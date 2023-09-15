a6af7f6a3dc09ecea2a4bb30f91cfb30124750ae
Naufal Athallah Iwel <spealdm6@gmail.com>
2023-09-15 15:08:34 +0700

    feat: browserstack config for cloud running

--- config/wdio.android.bs.conf.js
+++ config/wdio.android.bs.conf.js
@@ -1,17 +1,18 @@
-const path = require("path");
 const { config } = require("./wdio.shared.conf");
 
-config.port = 4723;
+config.user = process.env.BROWSERSTACK_USER;
+
+config.key = process.env.BROWSERSTACK_KEY;
 
 config.specs = ["./test/specs/android/add-note-screen*.js"];
 
 config.capabilities = [
   {
     "appium:platformName": "Android",
-    "appium:deviceName": "Nexus 5 API 33 Tiramisu",
-    "appium:platformVersion": "13",
+    "appium:deviceName": "Google Pixel 3",
+    "appium:platformVersion": "10.0",
     "appium:automationName": "UIAutomator2",
-    "appium:app": path.join(process.cwd(), "app/android/ColorNote+Notepad.apk"),
+    "appium:app": "bs://1e802325b2d0f5fb87e1f2dff1f44a2512746db",
     "appium:autoGrantPermissions": true,
   },
 ];
@@ -21,4 +22,4 @@ config.capabilities = [
 // Services take over a specific job you don't want to take care of. They enhance
 // your test setup with almost no effort. Unlike plugins, they don't add new
 // commands. Instead, they hook themselves up into the test process.
-(config.services = ["appium"]), (exports.config = config);
+(config.services = ["browserstack"]), (exports.config = config);
--- config/wdio.ios.conf.js
+++ config/wdio.ios.conf.js
@@ -16,4 +16,9 @@ config.capabilities = [
   },
 ];
 
-exports.config = config;
+//
+// Test runner services
+// Services take over a specific job you don't want to take care of. They enhance
+// your test setup with almost no effort. Unlike plugins, they don't add new
+// commands. Instead, they hook themselves up into the test process.
+(config.services = ["appium"]), (exports.config = config);
