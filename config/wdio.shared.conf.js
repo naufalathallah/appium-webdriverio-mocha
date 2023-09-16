3688061174dcba265fb009afda0a3ac9d8aa8449
Naufal Athallah Iwel <spealdm6@gmail.com>
2023-09-16 09:21:23 +0700

    fix: update allure when step fail screenshot

--- config/wdio.shared.conf.js
+++ config/wdio.shared.conf.js
@@ -224,8 +224,15 @@ exports.config = {
    * @param {boolean} result.passed    true if test has passed, otherwise false
    * @param {object}  result.retries   information about spec related retries, e.g. `{ attempts: 0, limit: 0 }`
    */
-  // afterTest: function(test, context, { error, result, duration, passed, retries }) {
-  // },
+  afterTest: async function (
+    test,
+    context,
+    { error, result, duration, passed, retries }
+  ) {
+    if (error) {
+      await browser.takeScreenshot();
+    }
+  },
 
   /**
    * Hook that gets executed after the suite has ended
