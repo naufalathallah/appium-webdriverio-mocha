35b5cca7781b06dbddf1d7f0739234ec73ab9c19
Naufal A <spealdm6@gmail.com>
2023-10-15 15:47:43 +0700

    feat: setup run on iphone 15 emulator

--- config/wdio.ios.conf.js
+++ config/wdio.ios.conf.js
@@ -8,11 +8,10 @@ config.specs = ["../test/specs/ios/*.js"];
 config.capabilities = [
   {
     platformName: "ios",
-    "appium:deviceName": "iPhone 12",
-    "appium:platformVersion": "14.5",
+    "appium:deviceName": "iPhone 15",
+    "appium:platformVersion": "17.0",
     "appium:automationName": "XCUITest",
-    "appium:app": path.join(process.cwd(), "app/android/ColorNote+Notepad.apk"),
-    "appium:autoGrantPermissions": true,
+    "appium:app": path.join(process.cwd(), "app/ios/UIKitCatalog.app.zip"),
   },
 ];
 
@@ -21,17 +20,19 @@ config.capabilities = [
 // Services take over a specific job you don't want to take care of. They enhance
 // your test setup with almost no effort. Unlike plugins, they don't add new
 // commands. Instead, they hook themselves up into the test process.
-(config.services = [
+config.services = [
   [
     "appium",
     {
       args: {
-        address: "localhost",
+        // Explicitly define the port
         port: 4723,
         relaxedSecurity: true,
       },
       logpath: "./",
+      command: 'appium',
     },
   ],
-]),
-  (exports.config = config);
+];
+
+exports.config = config;
